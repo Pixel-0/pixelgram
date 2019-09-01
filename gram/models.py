c168727900f9d286e4ec642a7f492c41088a7c9f
Pixel-0 <tiny0pixel@gmail.com>
2019-09-02 02:19:16 +0300

    create and define profile model

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):    
+    bio = models.CharField(max_length = 100)
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, default=1)
+    
+
+    def save_profile(self):
+        self.save()
