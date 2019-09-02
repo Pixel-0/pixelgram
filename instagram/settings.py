a531083832537a055eab4aed7023181fefc45d9b
Pixel-0 <tiny0pixel@gmail.com>
2019-09-02 12:39:29 +0300

    remove superuser

--- instagram/settings.py
+++ instagram/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'timeline.apps.TimelineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
